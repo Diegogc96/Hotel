47f04673ade27ef05b4b5d3d0cd1ffa8331240a2
TafirolVencido <galindocabreradiego@gmail.com>
2023-10-02 10:52:09 -0300

    Inicio

---
+++ ProyectoFinal_Hotel/src/proyectofinal_hotel/ProyectoFinal_Hotel.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyectofinal_hotel;
+
+/**
+ *
+ * @author Diego
+ */
+public class ProyectoFinal_Hotel {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
